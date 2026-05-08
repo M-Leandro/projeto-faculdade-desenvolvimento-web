8c7d2173d795b4fa1e4f7a13dbeb9a70ac7f0d57
mleandro <leandromario983@gmail.com>
2026-05-08 16:41:46 -0300

    ajuste de detalhes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Espera todo o HTML carregar antes de rodar o script
 document.addEventListener("DOMContentLoaded", function() {
     
-    // Pega o formulário pelo ID que colocamos no HTML
+    // Pega o formulário pelo ID no HTML
     const form = document.getElementById("form-contato");
 
-    // "Escuta" o momento em que o botão de submit for clicado
+    // Pega o momento em que o botão de submit for clicado
     form.addEventListener("submit", function(event) {
         
-        // Evita que a página recarregue (comportamento padrão de formulários)
+        // Evita que a página recarregue
         event.preventDefault(); 
 
-        // Captura os valores que o usuário digitou e remove espaços em branco nas pontas (.trim())
+        // Captura os valores que o usuário digitou e remove espaços em branco nas pontas
         const nome = document.getElementById("nome").value.trim();
         const email = document.getElementById("email").value.trim();
         const mensagem = document.getElementById("mensagem").value.trim();
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
             return; // Para a execução do código aqui se der erro
         }
 
-        // 2. Validação simples do formato de e-mail (precisa ter @ e um ponto)
+        // 2. Validação simples do formato de e-mail
         if (!email.includes("@") || !email.includes(".")) {
             alert("⚠️ Por favor, insira um endereço de e-mail válido.");
             return;
